59ab1c730e89442c9021839b7769936f5373bd30
Harry Bevins <harrytjbevins@yahoo.co.uk>
2021-01-25 19:32:57 +0000

    removed old models with Batchnorm and reg

--- zT/models.py
+++ zT/models.py
@@ -7,7 +7,6 @@ class network_models():
         self.Inputs = tf.keras.layers.Input
         self.Dense = tf.keras.layers.Dense
         self.Dropout = tf.keras.layers.Dropout
-        self.BN = tf.keras.layers.BatchNormalization
 
     def basic_model(self, input_dim, output_dim, layer_sizes, activation,
             drop_val, output_activation):
@@ -15,32 +14,6 @@ class network_models():
         inputs = a0
         for layer_size in layer_sizes:
             outputs = self.Dense(layer_size, activation=activation)(a0)
-            outputs = self.Dropout(drop_val)(outputs)
-            a0 = outputs
-        outputs = self.Dense(output_dim, activation=output_activation)(a0)
-        model = self.Model(inputs, outputs)
-        return model
-
-    def basic_model_norm(self, input_dim, output_dim, layer_sizes, activation,
-            drop_val, output_activation):
-        a0 = self.Inputs(shape = (input_dim,))
-        inputs = a0
-        for layer_size in layer_sizes:
-            outputs = self.Dense(layer_size, activation=activation)(a0)
-            outputs = self.Dropout(drop_val)(outputs)
-            outputs = self.BN()(outputs)
-            a0 = outputs
-        outputs = self.Dense(output_dim, activation=output_activation)(a0)
-        model = self.Model(inputs, outputs)
-        return model
-
-    def basic_model_L2(self, input_dim, output_dim, layer_sizes, activation,
-            drop_val, output_activation):
-        a0 = self.Inputs(shape = (input_dim,))
-        reg = tf.keras.regularizers.L2(l2=0.1)
-        inputs = a0
-        for layer_size in layer_sizes:
-            outputs = self.Dense(layer_size, activation=activation, kernel_regularizer=reg)(a0)
             outputs = self.Dropout(drop_val)(outputs)
             a0 = outputs
         outputs = self.Dense(output_dim, activation=output_activation)(a0)
