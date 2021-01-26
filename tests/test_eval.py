9c8b535edef7452ef736b7bc203114ae23d4c0e7
Harry Bevins <harrytjbevins@yahoo.co.uk>
2021-01-26 12:24:05 +0000

    edited eval tests

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -6,11 +6,12 @@ import pytest
 params = [0.25, 30, 2, 0.056, 1.3, 2, 30]
 z = np.arange(10, 20, 100)
 
-def test_existing_default_dir():
-    res = evaluate(params, z=z)
+#def test_download_model():
+#    res = evaluate(params, z=z)
 
 def test_existing_dir():
-    res = evaluate(params, z=z, base_dir='xHI_250121/', xHI=True)
+    res = evaluate(params, z=z, base_dir='best_T/', xHI=True)
 
-#def test_download_model():
-#    res = evaluate(params, z=z, base_dir='rrrr/')
+def test_existing_default_dir():
+    os.rename('best_T/', 'model_dir')
+    res = evaluate(params, z=z)
