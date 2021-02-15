855e024d7e12e59bbd87cd5d9064ef34d4c72470
Harry Bevins <harrytjbevins@yahoo.co.uk>
2021-02-15 14:19:49 +0000

    update to evaluation testing

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -2,6 +2,7 @@ import numpy as np
 from globalemu.eval import evaluate
 from globalemu.downloads import download
 import os
+import shutil
 
 params = [0.25, 30, 2, 0.056, 1.3, 2, 30]
 z = np.arange(10, 20, 100)
@@ -9,9 +10,9 @@ z = np.arange(10, 20, 100)
 
 def test_existing_dir():
     if os.path.exists('T_release/'):
-        os.remove('T_release/')
+        shutil.rmtree('T_release/')
     if os.path.exists('xHI_release/'):
-        os.remove('xHI_release/')
+        shutil.rmtree('xHI_release/')
 
     download(False).model()
     download(True).model()
