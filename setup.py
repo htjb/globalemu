02038f41a5846407216876e2b4f4cf47625d2810
Harry Bevins <harrytjbevins@yahoo.co.uk>
2021-03-12 14:28:45 +0000

    adding -alpha to version number so that 1.0.0 can be released with the MNRAS preprint

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme(short=False):
 
 setup(
     name='globalemu',
-    version='1.0.0',
+    version='1.0.0-alpha',
     description='globalemu: Robust and Fast Global 21-cm Signal Emulation',
     long_description=readme(),
     author='Harry T. J. Bevins',
