eb930fc9f9b433aaa5ccddcc910a46dbf11ca7c6
Harry Bevins <harrytjbevins@yahoo.co.uk>
2021-03-12 17:16:20 +0000

    bumping version number again for potential gui fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme(short=False):
 
 setup(
     name='globalemu',
-    version='1.0.0-alpha.2',
+    version='1.0.0-alpha.3',
     description='globalemu: Robust and Fast Global 21-cm Signal Emulation',
     long_description=readme(),
     author='Harry T. J. Bevins',
