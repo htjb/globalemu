f9a77afc1b9715f30fe7323492b5a83dcf77d015
htjb <harrytjbevins@yahoo.co.uk>
2021-05-17 14:47:14 +0100

    restructured cmSim.py

--- globalemu/cmSim.py
+++ globalemu/cmSim.py
@@ -2,7 +2,7 @@ import numpy as np
 from globalemu.downloads import download
 
 
-class calc_signal:
+class calc_signal():
 
     r"""
 
@@ -28,9 +28,6 @@ class calc_signal:
 
         self.z = z
         self.base_dir = base_dir
-        self.deltaT, self.T_K, self.T_s, self.T_r = self.calc()
-
-    def calc(self):
 
         A10 = 2.85e-15  # s^-1
         kb = 1.381e-23  # m^2 kg s^-2 K^-1
@@ -46,11 +43,11 @@ class calc_signal:
         planck_h = 6.626e-34  # m^2 kg s^-1
         c = 3e8  # m/s
 
-        T_r = T_cmb0*(1+self.z)
+        self.T_r = T_cmb0*(1+self.z)
         z_ref = 40
         T_K_ref = 33.7340  # K
 
-        T_K = T_K_ref*((1+self.z)/(1+z_ref))**2
+        self.T_K = T_K_ref*((1+self.z)/(1+z_ref))**2
 
         Y = 0.274  # Helium abundance by mass
         rhoc = 1.36e11*(h/0.7)**2  # M_sol/cMpc^3
@@ -60,28 +57,26 @@ class calc_signal:
         Tstar = 0.068  # K
         try:
             t, kappa10_HH_data = np.loadtxt('kappa_HH.txt', unpack=True)
-        except FileNotFoundError:
+        except OSError:
             download().kappa()
             t, kappa10_HH_data = np.loadtxt('kappa_HH.txt', unpack=True)
 
-        kappa10_HH = np.interp(T_K, t, kappa10_HH_data)
+        kappa10_HH = np.interp(self.T_K, t, kappa10_HH_data)
 
-        xc = (nH*kappa10_HH*1e-6*Tstar)/(A10*T_r)
-        invT_s = (1/T_r + xc*(1/T_K))/(1+xc)
-        T_s = 1/invT_s
+        xc = (nH*kappa10_HH*1e-6*Tstar)/(A10*self.T_r)
+        invT_s = (1/self.T_r + xc*(1/self.T_K))/(1+xc)
+        self.T_s = 1/invT_s
 
         xHI = 1
         nu0 = 1420.4e6
 
-        Hz = (H0)*np.sqrt(omega_m*(1+self.z)**3)
+        Hz = H0*np.sqrt(omega_m*(1+self.z)**3)
 
         tau = (3*planck_h*c**3*A10*xHI*nH) / \
-            (32*np.pi*kb*T_s*nu0**2*(1+self.z)*Hz/(1+self.z))
+            (32*np.pi*kb*self.T_s*nu0**2*(1+self.z)*Hz/(1+self.z))
 
-        deltaT = (T_s-T_r)/(1+self.z)*(1-np.exp(-tau))
+        deltaT = (self.T_s-self.T_r)/(1+self.z)*(1-np.exp(-tau))
 
         norm_factor = np.load(self.base_dir + 'AFB_norm_factor.npy')
-        deltaT = deltaT/np.abs(deltaT).max()*np.abs(norm_factor)*1e3
-        np.savetxt(self.base_dir + 'AFB.txt', deltaT)
-
-        return deltaT, T_K, T_s, T_r
+        self.deltaT = deltaT/np.abs(deltaT).max()*np.abs(norm_factor)*1e3
+        np.savetxt(self.base_dir + 'AFB.txt', self.deltaT)
