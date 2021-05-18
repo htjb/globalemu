b8177a1a346d556d299b5f5e0605a00b00a9e151
htjb <harrytjbevins@yahoo.co.uk>
2021-05-18 11:05:46 +0100

    docstring added for gui_config.py

--- globalemu/gui_config.py
+++ globalemu/gui_config.py
@@ -1,13 +1,76 @@
+"""
+This function can be used to generate a configurate file for the GUI that
+is specific to a given trained model. The file gets saved into the supplied
+``base_dir`` which should contain the relevant trained model. The user
+also needs to supply a path to the ``data_dir`` that contains the relevant
+testing and training data. Additional arguments are described below.
+
+A GUI config file is required to be able to visualise the signals with the
+GUI and once generated the gui can be run from the command line
+
+.. code: bash
+
+    globalemu /path/to/base_dir/containing/model/and/config/
+
+"""
+
 import numpy as np
 import pandas as pd
 import os
 
 class config():
-    def __init__(self, base_dir, paramnames, data_dir, logs, xHI=False):
+
+    r"""
+
+    **Parameters:**
+
+        base_dir: **string**
+            | The path to the file containing the trained tensorflow model
+                that the user wishes to visualise with the GUI. Must end
+                in '/'.
+
+        paramnames: **list of strings**
+            | This should be a list of parameter names in the correct input
+                order. For example for the released global signal model this
+                would correspond to
+
+                .. code: python
+
+                    paramnames = [r'$\log(f_*)$', r'$\log(V_c)$',
+                                  r'$\log(f_X)$',
+                                  r'$\tau$', r'$\alpha$',
+                                  r'$\nu_\mathrm{min}$',
+                                  r'$R_\mathrm{mfp}$']
+
+                Latex strings can be provided as above.
+
+        data_dir: **string**
+            | The file path to the training and test data which is used to set
+                the y lims of the GUI graph and ranges/intervals of GUI
+                sliders.
+
+    **Kwargs:**
+
+        xHI: **Bool / default: False**
+            | If True then ``globalemu`` will act as if it is evaluating a
+                neutral fraction history emulator.
+
+        logs: **list / default: [0, 1, 2]**
+            | The indices corresponding to the astrophysical
+                parameters that
+                were logged during training. The default assumes
+                that the first three columns in "train_data.txt" are
+                :math:`{f_*}` (star formation efficiency),
+                :math:`{V_c}` (minimum virial circular velocity) and
+                :math:`{f_x}` (X-ray efficieny).
+                
+    """
+
+    def __init__(self, base_dir, paramnames, data_dir, **kwargs):
         self.base_dir = base_dir
         self.paramnames = paramnames
-        self.logs = logs
-        self.xHI = xHI
+        self.logs = kwarg.pop('logs', [0, 1, 2])
+        self.xHI = kwarg.pop('xHI', False)
 
         test_data = np.loadtxt(data_dir + 'test_data.txt')
         test_labels = np.loadtxt(data_dir + 'test_labels.txt')
